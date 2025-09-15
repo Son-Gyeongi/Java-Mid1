8d54478ab47f06753bd956335e88ceaf7010bf99
Son-Gyeongi <thsruddl77@gmail.com>
2025-09-15 21:10:37 +0900

    section2 5. java.lang 패키지 소개

---
+++ src/lang/LangMain.java
@@ -0,0 +1,12 @@
+package lang;
+
+// 원래는 import 해야하는데 java.lang 패키지는
+// 자바의 기본으로 자주 사용해서 생략해도 된다.
+// import java.lang.System;
+
+public class LangMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
