e3c655dca79e0902af75b20ef3d58a9423a4b316
Son-Gyeongi <thsruddl77@gmail.com>
2025-09-19 22:35:20 +0900

    section2 13. 문제와 풀이

---
+++ src/lang/object/testmy/Rectangle.java
@@ -0,0 +1,27 @@
+package lang.object.testmy;
+
+public class Rectangle {
+    private int width; // 넓이
+    private int height; // 높이
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Rectangle rectangle = (Rectangle) o;
+        return width == rectangle.width && height == rectangle.height;
+    }
+}
