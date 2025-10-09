f023cc9ec9f8e45a3dd845e77d16ba18ddd868ae
Son-Gyeongi <thsruddl77@gmail.com>
2025-10-09 14:07:55 +0900

    section7 61. 날짜와 시간의 핵심 인터페이스

---
+++ src/time/ChronoFieldMain.java
@@ -0,0 +1,18 @@
+package time;
+
+import java.time.temporal.ChronoField;
+
+// 날짜와 시간의 핵심 인터페이스
+public class ChronoFieldMain {
+
+    public static void main(String[] args) {
+        ChronoField[] values = ChronoField.values();
+        for (ChronoField value : values) {
+            System.out.println(value + ", range = " + value.range());
+        }
+
+        System.out.println("MONTH_OF_YEAR.range() = " + ChronoField.MONTH_OF_YEAR.range());
+        System.out.println("DAY_OF_MONTH.range() = " + ChronoField.DAY_OF_MONTH.range());
+
+    }
+}
