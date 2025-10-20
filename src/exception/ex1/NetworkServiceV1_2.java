081995020b293c6bd92f02cbd6265cfdbee8eb80
Son-Gyeongi <thsruddl77@gmail.com>
2025-10-20 22:36:44 +0900

    section10 87. 예외처리가 필요한 이유3 - 반환 값으로 예외 처리

---
+++ src/exception/ex1/NetworkServiceV1_2.java
@@ -0,0 +1,37 @@
+package exception.ex1;
+
+/*
+해당 프로그램은 사용자의 입력을 받고,
+입력 받은 문자를 외부 서버에 전송하는 프로그램이다.
+
+NetworkService - 전체적인 흐름 관리
+ */
+public class NetworkServiceV1_2 {
+
+    // 복잡한 NetworkClient 로직 해결
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.initError(data);
+
+        String connectResult = client.connect();
+        // 결과가 성공이 아니다 -> 오류
+        if (isError(connectResult)) {
+            // 오류 로그
+            System.out.println("[네트워크 오류 발생] 오류 코드: " + connectResult);
+            return;
+        }
+
+        String sendResult = client.send(data);
+        if (isError(sendResult)) {
+            System.out.println("[네트워크 오류 발생] 오류 코드: " + sendResult);
+            return;
+        }
+
+        client.disconnect();
+    }
+
+    private static boolean isError(String resultCode) {
+        return !resultCode.equals("success");
+    }
+}
